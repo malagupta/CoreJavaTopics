c2d581c889843713e4d832e60d8c112c2a0b00ea
Mala Gupta <mala.gupta@gmail.com>
2023-06-14 19:33:59 +0530

    Added code to demo local variable type inference

---
+++ src/main/java/com/ejg/local_var_type_inference/DemoLocalVariableTypeInference.java
@@ -0,0 +1,17 @@
+package com.ejg.local_var_type_inference;
+
+import java.time.LocalDateTime;
+
+public class DemoLocalVariableTypeInference {
+
+    void demo() {
+        var now = LocalDateTime.now();
+    }
+
+}
+
+
+
+
+
+
